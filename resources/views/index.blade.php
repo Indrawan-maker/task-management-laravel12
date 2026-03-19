256132b2a274f097ebd7b6b1fb8d496dedf453b4
Indrawan-maker <indrawanismail0@gmail.com>
2026-03-19 19:36:05 +0800

    automatic make route href and more complex name route

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,23 @@
-<div>Hello from index blade template
-</div>
+<h1>List tugas anda
+<h1/>
 <br>
 
-@isset($name)
 
-<div>thid from varibale routes string : {{ $name }}
+<div>
+    {{-- @if(count($tasks) > 0)  --}}
+        @forelse($tasks as $task)
+        <div>
+            <a href="{{ route('task.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+        </div>
+        <div>no : {{ $task->id }}</div>
+        <div>judul : {{ $task->title }}</div>
+        <div>deksripsi : {{ $task->description }}</div>
+        @empty
+        <div>there is no tasks!</div>
+        @endforelse
+     {{-- @else 
+        tidak ada tugas
+    
+    @endif --}}
 </div>
 
-@endisset
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,84 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+
+class Task
+{
+  public function __construct(
+    public int $id,
+    public string $title,
+    public string $description,
+    public ?string $long_description,
+    public bool $completed,
+    public string $created_at,
+    public string $updated_at
+  ) {
+  }
+}
+
+$tasks = [
+  new Task(
+    1,
+    'Buy groceries',
+    'Task 1 description',
+    'Task 1 long description',
+    false,
+    '2023-03-01 12:00:00',
+    '2023-03-01 12:00:00'
+  ),
+  new Task(
+    2,
+    'Sell old stuff',
+    'Task 2 description',
+    null,
+    false,
+    '2023-03-02 12:00:00',
+    '2023-03-02 12:00:00'
+  ),
+  new Task(
+    3,
+    'Learn programming',
+    'Task 3 description',
+    'Task 3 long description',
+    true,
+    '2023-03-03 12:00:00',
+    '2023-03-03 12:00:00'
+  ),
+  new Task(
+    4,
+    'Take dogs for a walk',
+    'Task 4 description',
+    null,
+    false,
+    '2023-03-04 12:00:00',
+    '2023-03-04 12:00:00'
+  ),
+];
+
+
+Route::get('/', function () use ($tasks) {
     return view('index',[
-        'name' => 'indras'
+        'tasks' => $tasks
     ]);
-});
+})->name('task.index');
 
-Route::get('/endpoint-rusak', function() {
-    return 'hello from enpoint hello and names as holla';
-})->name('holla');
+Route::get('/{id}', function($id) {
+    return "task with id : $id";
+})->name('task.show');
 
-Route::get('/hallo', function() {
-    return redirect()->route('holla');
-});
+// Route::get('/endpoint-rusak', function() {
+//     return 'hello from enpoint hello and names as holla';
+// })->name('holla');
 
-Route::get('user/{name}', function ($name) {
- return 'user adalah ' . $name;
-});
+// Route::get('/hallo', function() {
+//     return redirect()->route('holla');
+// });
 
-Route::fallback(function() {
-    return 'default error route not exist';
-});
+// Route::get('user/{name}', function ($name) {
+//  return 'user adalah ' . $name;
+// });
+
+// Route::fallback(function() {
+//     return 'default error route not exist';
+// });
+
